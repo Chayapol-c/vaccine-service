daeda3e5d2d7382c779fcdcbe75dee7db5e6e1db
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2024-04-30 21:10:27 +0700

    add retrieve all site route

--- src/main/kotlin/com/example/plugins/routes/ServiceSiteRoutes.kt
+++ src/main/kotlin/com/example/plugins/routes/ServiceSiteRoutes.kt
@@ -4,14 +4,14 @@ import com.example.models.ServiceSite
 import com.example.models.serviceSiteStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.serviceSiteRouting() {
-
-    route("/site") {
-        get("s"){
+    route("/sites") {
+        get{
             if (serviceSiteStorage.isNotEmpty()) {
                 call.respond(serviceSiteStorage)
             } else {
@@ -19,7 +19,8 @@ fun Route.serviceSiteRouting() {
             }
 
         }
-
+    }
+    route("/site") {
         get( "/{id?}" ) {
             val id = call.parameters["id"] ?: return@get call.respondText("Missing Id", status = HttpStatusCode.BadRequest)
             val serviceSite = serviceSiteStorage.find { it.id == id } ?: return@get call.respondText("No service site with id $id", status = HttpStatusCode.NotFound)
